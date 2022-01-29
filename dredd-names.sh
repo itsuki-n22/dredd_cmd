e862dc77d9979a1338dfe72994cf98b648816118
itsuki-n22 <54315732+itsuki-n22@users.noreply.github.com>
2022-01-29 17:17:55 +0900

    汎用性の向上

--- dredd-names.sh
+++ dredd-names.sh
@@ -1,15 +1,13 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 source ${SCRIPT_DIR}/params.cfg
 
-HOOKFILES="${SCRIPT_DIR}/hooks/*.js"
-
 # OASが分割されている場合 dredd は利用できないので
 # OASが分割されている場合mergeする。
 
 docker run -it --rm -v $SCRIPT_DIR/..:/app \
 properdom/swagger-merger swagger-merger \
 -i app/$FILE \
--o app/dredd/tmp/merged.yml
+-o app/$DREDD_DIR/tmp/merged.yml
 
 # APIがOAS通りのレスポンスを返しているか確認する。
 # 出力が長いので、詳細は ./dredd/dredd_result.txt に出力する。
@@ -17,6 +15,6 @@ properdom/swagger-merger swagger-merger \
 docker run -it --rm -v $SCRIPT_DIR/..:/openapi \
 --add-host=host.docker.internal:host-gateway \
 apiaryio/dredd sh -c \
-"dredd openapi/dredd/tmp/merged.yml $API_URL \
+"dredd openapi/$DREDD_DIR/tmp/merged.yml $API_URL \
 -d --names --no-color| \
 grep -e 'info\:' -e 'fail\:' -e 'complete\:'"
--- dredd.sh
+++ dredd.sh
@@ -1,23 +1,23 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 source ${SCRIPT_DIR}/params.cfg
 
-HOOKFILES="${SCRIPT_DIR}/hooks/*.js"
+HOOKFILES="/openapi/$DREDD_DIR/hooks/$HOOKFILES"
 
 # OASが分割されている場合 dredd は利用できないので
 # OASが分割されている場合mergeする。
 
-docker run -it --rm -v $SCRIPT_DIR:/app \
+docker run -it --rm -v $SCRIPT_DIR/..:/app \
 properdom/swagger-merger swagger-merger \
 -i app/$FILE \
--o app/dredd/merged.yml
+-o app/$DREDD_DIR/tmp/merged.yml
 
 # APIがOAS通りのレスポンスを返しているか確認する。
 # 出力が長いので、詳細は ./dredd/dredd_result.txt に出力する。
 
-docker run -it --rm -v $SCRIPT_DIR:/openapi \
+docker run -it --rm -v $SCRIPT_DIR/..:/openapi \
 --add-host=host.docker.internal:host-gateway \
 apiaryio/dredd sh -c \
-"dredd openapi/dredd/merged.yml $API_URL \
+"dredd openapi/$DREDD_DIR/tmp/merged.yml $API_URL \
 -d --hookfiles=$HOOKFILES --no-color | \
-tee openapi/dredd/dredd_result.txt | \
+tee openapi/$DREDD_DIR/tmp/dredd_result.txt | \
 grep -e 'pass\:' -e 'fail\:' -e 'complete\:'"
